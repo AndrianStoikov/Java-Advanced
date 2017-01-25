6445934c9bf2f3ba313f63285dd96c0a15487e8d
AndrianStoikov <sandi6842@gmail.com>
2017-01-25 22:11:24 +0200

    Solution 100/100

---
+++ Sets and Maps - Exercise/src/SetsofElements.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashSet;
+
+
+public class SetsofElements {
+    public static void main(String[] args) {
+        BufferedReader Console = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedHashSet<String> names = new LinkedHashSet<>();
+        try {
+            Integer n = Integer.parseInt(Console.readLine());
+            for (int i = 0; i < n; i++) {
+                names.add(Console.readLine());
+            }
+
+            names.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
